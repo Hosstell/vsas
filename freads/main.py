95c18f491a6f88224dfed8a76e9a3d4e11b2fc4e
Andrey <st.andrey-serov@yandex.ru>
2023-08-02 23:13:39 +0300

    added new arg - Show reverse

--- freads/main.py
+++ freads/main.py
@@ -9,13 +9,14 @@ import os
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-f", "--filename", type=str, required=True, help="Input .sam file")
 argParser.add_argument("-c", "--count", type=int, default=1, help="Сount nucleotides in start of a read")
+argParser.add_argument("-r", "--reverse", action='store_true', help="Show reverse?")
 args = argParser.parse_args()
 
 COUNT_NUCLEOTIDE_IN_START = args.count
 FILENAME = args.filename
+IS_REVERSE = args.reverse
 
-
-def get_reads_info(filename, count_nucleotide_in_start):
+def get_reads_info_forward(filename, count_nucleotide_in_start):
     res_ = {}
     with open(filename, 'r') as file:
         for line in file:
@@ -25,8 +26,10 @@ def get_reads_info(filename, count_nucleotide_in_start):
             if line.split('	')[2] == '*':
                 continue
 
-            seq = line.split('	')[9]
+            if line.split('	')[1] == '16':
+                continue
 
+            seq = line.split('	')[9]
             len_seq = len(seq)
 
             if len_seq in res_:
@@ -36,12 +39,51 @@ def get_reads_info(filename, count_nucleotide_in_start):
     return res_
 
 
+def get_reads_info_reverse(filename, count_nucleotide_in_start):
+    res_ = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            if line[0] == '@':
+                continue
+
+            if line.split('	')[2] == '*':
+                continue
+
+            if line.split('	')[1] == '0':
+                continue
+
+            seq = line.split('	')[9]
+
+            seq = seq[::-1].lower()
+            replaces = {
+                't': 'A',
+                'a': 'T',
+                'g': 'C',
+                'c': 'G'
+            }
+            for i, j in replaces.items():
+                seq = seq.replace(i, j)
+
+            len_seq = len(seq)
+
+            if len_seq in res_:
+                res_[len_seq] += Counter(seq[:count_nucleotide_in_start])
+            else:
+                res_[len_seq] = Counter(seq[:count_nucleotide_in_start])
+    return res_
+
+def get_reads_info(filename, count_nucleotide_in_start, is_reverse):
+    if is_reverse:
+        return get_reads_info_reverse(filename, count_nucleotide_in_start)
+    else:
+        return get_reads_info_forward(filename, count_nucleotide_in_start)
+
 def get_reads_info_debug():
     devdata = json.loads(open("devdata.json", "r").read())
     return {int(k): v for k, v in devdata.items()}
 
 
-def save_graph(read_info, filename):
+def save_graph(read_info, filename, direction):
     for k in read_info.keys():
         read_info[k] = {n: read_info[k][n]/sum(read_info[k].values())*100 for n in dict(read_info[k]).keys()}
 
@@ -49,8 +91,7 @@ def save_graph(read_info, filename):
     res = [x for x in res if 13 <= x[0] <= 30]
     res.sort()
     lens_names = [x for x, _ in res]
-    nuk_names = ['A', 'T', 'G', 'C', 'N']
-    colors = {'A': 'green', 'T': 'red', 'C': 'blue', 'G': 'yellow', 'N': 'black'}
+    nuk_names = ['A', 'C', 'G', 'T']
 
     fig, ax = plt.subplots()
     bottom = np.zeros(len(lens_names))
@@ -69,15 +110,18 @@ def save_graph(read_info, filename):
         ax.bar_label(p, fmt='%.1f', label_type='center', size=7)
 
     ax.legend(bbox_to_anchor=(1, 1.02), loc="upper left")
-    ax.set_title(f'{FILENAME} (-c {COUNT_NUCLEOTIDE_IN_START})')
+    ax.set_title(f'{FILENAME} (-c {COUNT_NUCLEOTIDE_IN_START}, {direction})')
 
     plt.subplots_adjust(right=0.87)
     plt.savefig(filename)
 
 
 if __name__ == '__main__':
+    direction = 'reverse' if IS_REVERSE else 'forward'
+    filename = f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.{direction}.png'
+
     if os.getenv('DEV'):
         reads_info = get_reads_info_debug()
     else:
-        reads_info = get_reads_info(FILENAME, COUNT_NUCLEOTIDE_IN_START)
-    save_graph(reads_info, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.png')
+        reads_info = get_reads_info(FILENAME, COUNT_NUCLEOTIDE_IN_START, IS_REVERSE)
+    save_graph(reads_info, filename, direction)
