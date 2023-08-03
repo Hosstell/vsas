08a7203141e5b128483bddd85f5ab3aebf7c01a6
Andrey <st.andrey-serov@yandex.ru>
2023-08-03 21:43:00 +0300

    added generation of 6 pics: 5f, 5r, 5b, 3f, 3r, 3b

--- freads/main.py
+++ freads/main.py
@@ -1,5 +1,6 @@
 import json
 from collections import Counter
+from typing import List, Tuple, Literal
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,86 +10,76 @@ import os
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-f", "--filename", type=str, required=True, help="Input .sam file")
 argParser.add_argument("-c", "--count", type=int, default=1, help="Сount nucleotides in start of a read")
-argParser.add_argument("-r", "--reverse", action='store_true', help="Show reverse?")
 args = argParser.parse_args()
 
 COUNT_NUCLEOTIDE_IN_START = args.count
 FILENAME = args.filename
-IS_REVERSE = args.reverse
 
-def get_reads_info_forward(filename, count_nucleotide_in_start):
-    res_ = {}
+def get_complementarity(seq: str) -> str:
+    seq = seq.lower()
+    replaces = {
+        't': 'A',
+        'a': 'T',
+        'g': 'C',
+        'c': 'G'
+    }
+    for i, j in replaces.items():
+        seq = seq.replace(i, j)
+    return seq
+
+def get_read_info(filename):
+    f5 = {}
+    f3 = {}
+    r5 = {}
+    r3 = {}
+
     with open(filename, 'r') as file:
         for line in file:
             if line[0] == '@':
                 continue
 
             if line.split('	')[2] == '*':
+                continue
+
+            seq = line.split('	')[9]
+            l = len(seq)
+
+            if line.split('	')[1] == '0':
+                if l in f5:
+                    f5[l] += Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+                else:
+                    f5[l] = Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+
+                seq = get_complementarity(seq[::-1])
+                if l in r3:
+                    r3[l] += Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+                else:
+                    r3[l] = Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+
                 continue
 
             if line.split('	')[1] == '16':
+                if l in f3:
+                    f3[l] += Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+                else:
+                    f3[l] = Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+
+                seq = get_complementarity(seq[::-1])
+                if l in r5:
+                    r5[l] += Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+                else:
+                    r5[l] = Counter(seq[:COUNT_NUCLEOTIDE_IN_START])
+
                 continue
 
-            seq = line.split('	')[9]
-            len_seq = len(seq)
+    return f5, f3, r5, r3
 
-            if len_seq in res_:
-                res_[len_seq] += Counter(seq[:count_nucleotide_in_start])
-            else:
-                res_[len_seq] = Counter(seq[:count_nucleotide_in_start])
-    return res_
+def save_graph(reads_info, graph_name):
+    for k in reads_info.keys():
+        reads_info[k] = {n: reads_info[k][n]/sum(reads_info[k].values())*100 for n in dict(reads_info[k]).keys()}
 
-
-def get_reads_info_reverse(filename, count_nucleotide_in_start):
-    res_ = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            if line[0] == '@':
-                continue
-
-            if line.split('	')[2] == '*':
-                continue
-
-            if line.split('	')[1] == '0':
-                continue
-
-            seq = line.split('	')[9]
-
-            seq = seq[::-1].lower()
-            replaces = {
-                't': 'A',
-                'a': 'T',
-                'g': 'C',
-                'c': 'G'
-            }
-            for i, j in replaces.items():
-                seq = seq.replace(i, j)
-
-            len_seq = len(seq)
-
-            if len_seq in res_:
-                res_[len_seq] += Counter(seq[:count_nucleotide_in_start])
-            else:
-                res_[len_seq] = Counter(seq[:count_nucleotide_in_start])
-    return res_
-
-def get_reads_info(filename, count_nucleotide_in_start, is_reverse):
-    if is_reverse:
-        return get_reads_info_reverse(filename, count_nucleotide_in_start)
-    else:
-        return get_reads_info_forward(filename, count_nucleotide_in_start)
-
-def get_reads_info_debug():
-    devdata = json.loads(open("devdata.json", "r").read())
-    return {int(k): v for k, v in devdata.items()}
-
-
-def save_graph(read_info, filename, direction):
-    for k in read_info.keys():
-        read_info[k] = {n: read_info[k][n]/sum(read_info[k].values())*100 for n in dict(read_info[k]).keys()}
-
-    res = [(int(k), v) for k, v in read_info.items()]
-    res = [x for x in res if 13 <= x[0] <= 30]
+    res = [(int(k), v) for k, v in reads_info.items()]
+    res = [x for x in res if 18 <= x[0] <= 30]
     res.sort()
     lens_names = [x for x, _ in res]
     nuk_names = ['A', 'C', 'G', 'T']
@@ -98,7 +89,7 @@ def save_graph(read_info, filename, direction):
     width = 0.9
 
     for nuk in nuk_names:
-        values = [dict(read_info[l]).get(nuk, 0) for l in lens_names]
+        values = [dict(reads_info[l]).get(nuk, 0) for l in lens_names]
         p = ax.bar(
             list(map(str, lens_names)),
             values,
@@ -110,18 +101,21 @@ def save_graph(read_info, filename, direction):
         ax.bar_label(p, fmt='%.1f', label_type='center', size=7)
 
     ax.legend(bbox_to_anchor=(1, 1.02), loc="upper left")
-    ax.set_title(f'{FILENAME} (-c {COUNT_NUCLEOTIDE_IN_START}, {direction})')
+    ax.set_title(graph_name)
 
     plt.subplots_adjust(right=0.87)
-    plt.savefig(filename)
+    plt.savefig(f'{graph_name}.png')
 
 
 if __name__ == '__main__':
-    direction = 'reverse' if IS_REVERSE else 'forward'
-    filename = f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.{direction}.png'
+    f5, f3, r5, r3 = get_read_info(FILENAME)
 
-    if os.getenv('DEV'):
-        reads_info = get_reads_info_debug()
-    else:
-        reads_info = get_reads_info(FILENAME, COUNT_NUCLEOTIDE_IN_START, IS_REVERSE)
-    save_graph(reads_info, filename, direction)
+    b5 = {i: f5[i] + r5[i] for i in range(18, 31)}
+    b3 = {i: f3[i] + r3[i] for i in range(18, 31)}
+
+    save_graph(f5, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.5f')
+    save_graph(f3, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.3f')
+    save_graph(r5, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.5r')
+    save_graph(r3, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.3r')
+    save_graph(b5, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.5b')
+    save_graph(b3, f'{FILENAME}.c{COUNT_NUCLEOTIDE_IN_START}.3b')
