d607dcf045b9b7bc01b5156cab7eb98678697c6e
Andrey <st.andrey-serov@yandex.ru>
2023-08-13 20:51:04 +0300

    freads: refactoring

--- freads/cicd/build_image_and_push_dockerhub.sh
+++ freads/cicd/build_image_and_push_dockerhub.sh
@@ -1,13 +1,20 @@
 set -e
 
 cd ./freads
+
+VERSION=$(cat version.txt)
+docker manifest inspect vsas/freads:$VERSION > /dev/null
+if $?; then
+  exit 0
+fi
+
 docker build -t freads .
 
 echo "Deploying latest version"
 docker image tag freads vsas/freads:latest
 docker image push vsas/freads:latest
 
-VERSION=$(cat version.txt)
+
 echo "Deploying ${VERSION} version"
 docker image tag freads vsas/freads:${VERSION}
 docker image push vsas/freads:${VERSION}
@@ -16,4 +23,4 @@ docker image push vsas/freads:${VERSION}
 echo "Sending notification"
 cd ..
 pip3 install -r ./utils/requirements.txt
-python3 ./utils/telegram_notification.py -t ${{ secrets.TELEGRAM_BOT_TOKEN }} -c ${{ secrets.TELEGRAM_CHAT_ID_FOR_NOTIFICATIONS }} -m "$(cat ./freads/README.md)"
+python3 ./utils/telegram_notification.py -t $TELEGRAM_BOT_TOKEN -c $TELEGRAM_CHAT_ID_FOR_NOTIFICATIONS -m "$(cat ./freads/README.md)"
